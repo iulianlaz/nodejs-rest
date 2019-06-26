7bbd4de6b150b3326702fc4118e20e493fd2f351
iulianlaz <iulianlaz@gmail.com>
2019-06-26 17:19:30 +0300

    Small changes.

--- store.ts
+++ store.ts
@@ -2,16 +2,18 @@ let mongoose = require('mongoose');
 
 export class Store {
 
+    private _host = 'mongodb://192.168.33.20/test';
+
     private _client: Object = {};
 
     constructor() {
-        mongoose.connect('mongodb://192.168.33.20/test', {useNewUrlParser: true});
+        mongoose.connect(this._host, {useNewUrlParser: true});
 
         var db = mongoose.connection;
         db.on('error', console.error.bind(console, 'connection error:'));
         db.once('open', () => {
             // we're connected!
-            console.log('connected');
+            console.log('Connected to database');
         });
 
         this._client = mongoose;
